39bf272e9a6004a2633c253b4e08443702aaa828
Marmik Mewada <95102437+marmikmewada@users.noreply.github.com>
2022-11-27 17:54:44 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const itemInput = document.querySelector("#itemInput");
 
 const tempListArea = document.querySelector(".temp-list-area");
 let tempItemList = [];
+let finalItemList = [];
 
 newItemButton.addEventListener("click", function(){
     function addGrocery (item){
@@ -16,19 +17,32 @@ newItemButton.addEventListener("click", function(){
         let li = document.createElement("li");
         tempListArea.appendChild(li);
         li.innerHTML = `<span>${itemInput.value}</span><i class="fa-solid fa-xmark"></i>`;
-        
+        itemInput.value="";
+
         const newi= document.querySelectorAll(".temp-list-area>li>i");
         // console.log(newi);
         newi.forEach((value,index)=>{
-            newi[index].addEventListener("click", function(){
-                // tempItemList.splice(index,1);
+            newi[index].addEventListener("click", function(e){
+                e.stopImmediatePropagation();
+                console.log(index);
+                tempItemList.splice(index,1);
                 // console.log(tempItemList);
-                // tempListArea.removeChild(index);
-                // console.log(index);
-            })
+                // console.log(tempListArea);
+                tempListArea.removeChild(tempListArea.children[index]);
+                // li.tempListArea.removeChild(tempListArea.children[index]);
+            });
         });
+        sendFinalButton.addEventListener("click", function(){
+            finalItemList = [...tempItemList];
+            tempItemList.forEach((value, index)=>{
+                tempItemList.splice(index,tempItemList.length);
+            });
+            
+        })
+        // console.log(finalItemList);
+        // console.log(tempItemList);
         
-    })
+    });
 
 
 
